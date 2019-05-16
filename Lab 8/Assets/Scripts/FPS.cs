f96d5a660b9f931b12403d677c88ac42d94e12fc
Marco Fuentes <marcofuentes1607@gmail.com>
2019-05-15 22:36:04 -0600

    Requerimientos cumplidos

---
+++ Lab 8/Assets/Scripts/FPS.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+    public Light luz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        luz.intensity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        luz.intensity = luz.intensity + 0.5f * Mathf.Sin(Time.time);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        luz.intensity = 0;
+    }
+}
---
+++ Lab 8/Assets/Scripts/RayScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayScript : MonoBehaviour
+{
+    public Light luz;
+    public AudioSource clip;
+    bool isOn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray myRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hitInfo;
+        transform.Find("Canvas").gameObject.SetActive(false);
+        if (Physics.Raycast(myRay, out hitInfo, 5))
+        {
+            if (!hitInfo.collider.CompareTag("nada"))
+            {
+                transform.Find("Canvas").gameObject.SetActive(true);
+                transform.Find("Canvas").transform.Find("Text").gameObject.GetComponent<Text>().text = hitInfo.collider.tag;
+            }
+            else
+            {
+                transform.Find("Canvas").gameObject.SetActive(false);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            clip.Play();
+            if (isOn)
+            {
+                isOn = false;
+                luz.intensity = 0;
+            }
+            else
+            {
+                isOn = true;
+                luz.intensity = 3;
+            }
+        }
+
+    }
+}
